6fc8ec868dd616fa33f69c7482e8b0aeeda12980
zyjava <zyjava@qq.com>
2022-06-24 18:20:54 +0800

    完成了商品列表页面的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,71 @@
+<template>
+	<view>
+		<view class="goods-item">
+			<!-- 左 -->
+			<view class="goods-item-left">
+				<image :src="good.goods_small_logo || defaultPic" class="goods-pic"></image>
+			</view>
+			<!-- 右 --> 
+			<view class="goods-item-right">
+				<view class="goods-name">{{good.goods_name}}</view>
+				<view class="goods-info-box">
+					<view class="goods-price">￥{{good.goods_price | tofixed}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+ 
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			good:{
+				type:Object,
+				default:{}
+			}
+		},
+		data() {
+			return {
+				defaultPic:'http://image2.suning.cn/uimg/b2c/newcatentries/0070078057-000000000634917020_1_400x400.jpg'
+			};
+		},
+		filters:{
+			tofixed(num){
+				return Number(num).toFixed(2); 
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	  .goods-item{
+		  display: flex;
+		  padding: 10px 5px;
+		  border-bottom: 1px solid #DDDDDD;
+		  
+		  .goods-item-left{
+			  margin-right: 5px;
+			  image{
+				  width: 100px;
+				  height: 100px;
+				  display: block;
+				  
+			  }
+		  }
+		  .goods-item-right{
+			  display: flex;
+			  flex-direction: column;
+			  justify-content: space-between;
+			  .goods-name{
+				  font-size: 13px;
+			  }
+			  .goods-info-box{
+				  .goods-price{
+					  color: #C00000;
+					  font-size: 16px;
+				  }
+			  }
+		  }
+	  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,19 +1,68 @@
 <template>
 	<view>
-		goods_list页面
+		<view class="goods-list">
+			<view v-for="(good,i) in goodsList" :key="i" @click="gotoDetail(good)">
+				<my-goods :good="good"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
-
+    
 <script>
 	export default {
 		data() {
 			return {
-				
+				queryObj:{
+					query:'',
+					cid:'',
+					pagenum:1,
+					pagesize:10 
+				},
+				goodsList:[],
+				total:0,
+				isloading:false
 			};
-		}
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || '';
+			this.queryObj.cid = options.cid || '';
+			
+			this.getGoodsList();
+		},
+		onReachBottom() {
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg("我是有底线的！！")
+			if(this.isloading) return;
+			this.queryObj.pagenum++;
+			this.getGoodsList(); 
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1;
+			this.total = 0;
+			this.isloading = false;
+			this.goodsList = [];
+			
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+		},
+		methods:{
+			async getGoodsList(cb){
+				this.isloading = true;
+				const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj);
+				this.isloading = false;
+				cb && cb();
+				console.log(res); 
+				if (res.meta.status !== 200) return uni.$showMsg();
+				this.goodsList = [...this.goodsList,...res.message.goods];
+				this.total = res.message.total;
+			},
+			gotoDetail(e){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id='+e.goods_id      
+				})  
+			}
+		} 
 	}
 </script>
 
 <style lang="scss">
-
+  
 </style>
